942237ce67512e757b9468b91787684f9c2413a1
romthi38 <thibault.romanin@gmail.com>
2015-09-11 14:08:15 +0200

    Création du fichier generationpdf.h
    C'est la classe de génération du Pdf.

---
+++ generationpdf.h
@@ -0,0 +1,19 @@
+/**
+* @file generationpdf.h
+* @brief Classe de GenerationPdf
+* @author tromanin
+* @author rchauveau
+* @date vendredi 11 septembre 2015, 14:04:00
+* @class GenerationPdf
+*/
+
+#ifndef GENERATIONPDF_H
+#define GENERATIONPDF_H
+
+class GenerationPdf
+{
+public:
+    GenerationPdf();
+};
+
+#endif // GENERATIONPDF_H
