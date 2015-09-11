4e2c0719ed6cab80c6ad7f8db0b756b6206c625a
RChauveau <rm.chauveau@gmail.com>
2015-09-11 14:30:42 +0200

    Ajout du jeu d'essais ptsDeVente

---
+++ ptsDeVente.sql
@@ -0,0 +1,6 @@
+-- Jeu d'essais Point de Vente
+delete from ptsDeVente;
+INSERT INTO ptsDeVente VALUES 
+(1,'gap nord','fruit','Paul','Pierre','4514578482','ppi@mail.com','rue du ...',' ','05000','gap'),
+(2,'gap sud','viande','Jean','Pierre','4514584482','jpi@mail.com','rue du ...',' ','05000','gap'),
+(3,'Sisteron','pommes','Mireille','Dupont','4514578482','md@mail.com','rue du .',' ','04200','sisteron');
