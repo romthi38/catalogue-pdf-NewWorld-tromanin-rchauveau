b643b871c672e83e8d8a8d23375d5a7b27f43d11
romthi38 <thibault.romanin@gmail.com>
2015-09-11 14:10:33 +0200

    Création du fichier generationpdf.cpp
    Ce fichier génère le fichier Pdf.

---
+++ generationpdf.cpp
@@ -0,0 +1,13 @@
+/**
+* @file generationpdf.cpp
+* @brief Fichier de génération des Pdf
+* @author tromanin
+* @author rchauveau
+* @date vendredi 11 septembre 2015, 14:04:00
+*/
+
+#include "generationpdf.h"
+
+GenerationPdf::GenerationPdf()
+{
+}
