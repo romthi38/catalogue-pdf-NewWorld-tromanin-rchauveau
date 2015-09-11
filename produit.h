e13965ac990ce63cd2ede9ed3856e5bcb7c01d58
RChauveau <rm.chauveau@gmail.com>
2015-09-11 16:40:36 +0200

    Ajout de la classe rayon

--- produit.h
+++ produit.h
@@ -10,10 +10,48 @@
 #ifndef PRODUIT_H
 #define PRODUIT_H
 
+#include <QString>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @class Classe Produit
+ * @brief Classe représentant le produit
+ * Private :
+ * nom du produit
+ * libelle du produit
+ * Public :
+ * Constructeur Produit
+ * @fn getLibelleProduit
+ * @brief retourne le libelle du produit
+ * @fn getPrixUnitaireLot
+ * @brief retourne la prix unitaire du produit
+ * @fn getQuantiteLot
+ * @brief retourne la quantite du lot
+ * @fn getDateLimiteConsomationLot
+ * @brief retourne la date limite de consomation
+ */
 class Produit
 {
+private:
+    int noProduit;
+    QString libelleProduit;
+
 public:
+    //constructeur
+    Produit(int n, QString lib);
     Produit();
+
+    //retourne le libelle du produit
+    QString getLibelleProduit();
+    //retourne le pix unitaire du produit
+    int getPrixUnitaireLot();
+    //retourne la quantite du lot
+    int getQuantiteLot();
+    //retourne la date limite de consomation
+    QDate getDateLimiteConsomationLot();
+
 };
 
 #endif // PRODUIT_H
