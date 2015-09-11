ad06ee5a6086ff1bcc5e6574a34b227c28075656
RChauveau <rm.chauveau@gmail.com>
2015-09-11 14:23:00 +0200

    Ajout du jeu d'essais utilisateur

---
+++ utilisateur_data.sql
@@ -0,0 +1,4 @@
+insert into utilisateur values
+(1,"romanin","tibault","tromanin","rue1","rue2","05000","tromanin@gmail.com","0456985125","","GAP"),
+(2,"chauveau","remi","rchauveau","rue1","rue2","05260","rchauveau@gmail.com","0569762147","","ST JEAN ST NICOLAS"),
+(3,"jean","pierre","pjean","rue1","rue2","05000","pjean@gmail.com","0490651245","","GAP");
