313e25a42e00d706255f7af9fdffe9788f051867
RChauveau <rm.chauveau@gmail.com>
2015-09-11 15:06:34 +0200

    Ajout de la connexion sql

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <QCoreApplication>
+#include <QSqlDatabase>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     
+    // Connexion à la base de données
+    QSqlDatabase * baseBornes;
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    baseBornes = new QSqlDatabase(db);
+    baseBornes->setHostName("localhost");
+    baseBornes->setUserName("root");
+    baseBornes->setDatabaseName("backOfficeNW");
+    baseBornes->setPassword("passf005");
+    baseBornes->open();
+
     return a.exec();
 }
