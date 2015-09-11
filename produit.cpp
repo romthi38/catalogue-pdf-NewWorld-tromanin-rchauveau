256abc4e88fde38128f8ab2f0060fce04c80d9c1
RChauveau <rm.chauveau@gmail.com>
2015-09-11 16:45:34 +0200

    Ajout de produit.cpp

--- produit.cpp
+++ produit.cpp
@@ -8,6 +8,30 @@
 
 #include "produit.h"
 
+//constructeur
+Produit::Produit(int n, QString lib)
+{
+    noProduit = no;
+    libelleProduit = lib;
+}
 Produit::Produit()
 {
 }
+
+
+Produit::getLibelleProduit()
+{
+}
+
+Produit::getPrixUnitaireLot()
+{
+}
+
+Produit::getQuantiteLot()
+{
+}
+
+Produit::getDateLimiteConsomationLot()
+{
+}
+
