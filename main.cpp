04c26d020e5b97cee200a6fc1dfae18af868f06f
RChauveau <rm.chauveau@gmail.com>
2015-09-18 14:49:47 +0200

    Ajout du script de connexion à la db

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     
-    // Connexion à la base de données
+    // Connexion à la base de donnée
     QSqlDatabase * baseBornes;
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
     baseBornes = new QSqlDatabase(db);
-    baseBornes->setHostName("localhost");
-    baseBornes->setUserName("root");
-    baseBornes->setDatabaseName("backOfficeNW");
-    baseBornes->setPassword("passf005");
+    baseBornes->setHostName("172.16.63.111");
+    baseBornes->setUserName("tromanin");
+    baseBornes->setDatabaseName("dbtromaninNewWorld");
+    baseBornes->setPassword("PPhdu14Zer");
     baseBornes->open();
 
     return a.exec();
